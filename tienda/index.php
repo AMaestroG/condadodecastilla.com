fd7f1941c371dd13ba7dc845b614ee47d618caac
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-07 08:55:42 -0300

    Unify styles into single CSS

--- tienda/index.php
+++ tienda/index.php
@@ -11,7 +11,6 @@ require_once __DIR__ . '/../includes/auth.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda</title>
     <link rel="stylesheet" href="/assets/css/epic_theme.css">
-    <?php require_once __DIR__ . '/../includes/load_page_css.php'; ?>
 </head>
 <body>
 <?php require_once __DIR__ . '/../_header.html'; ?>
