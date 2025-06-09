3947b3ab34243af4ddabf1c4060baee21917fe0f
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-08 22:26:29 -0300

    Add dependency setup script and install instructions

---
+++ scripts/setup_frontend_libs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+JS_DIR="assets/vendor/js"
+CSS_DIR="assets/vendor/css"
+
+mkdir -p "$JS_DIR" "$CSS_DIR"
+
+# jQuery
+JQUERY_VERSION="3.7.1"
+JQUERY_URL="https://code.jquery.com/jquery-${JQUERY_VERSION}.min.js"
+
+curl -L "$JQUERY_URL" -o "$JS_DIR/jquery.min.js"
+
+# Bootstrap
+BOOTSTRAP_VERSION="5.3.2"
+BOOTSTRAP_BASE="https://cdn.jsdelivr.net/npm/bootstrap@${BOOTSTRAP_VERSION}/dist"
+
+curl -L "$BOOTSTRAP_BASE/js/bootstrap.bundle.min.js" -o "$JS_DIR/bootstrap.bundle.min.js"
+curl -L "$BOOTSTRAP_BASE/css/bootstrap.min.css" -o "$CSS_DIR/bootstrap.min.css"
+
+cat <<MSG
+Librerías descargadas en:
+  - $JS_DIR/jquery.min.js
+  - $JS_DIR/bootstrap.bundle.min.js
+  - $CSS_DIR/bootstrap.min.css
+MSG
