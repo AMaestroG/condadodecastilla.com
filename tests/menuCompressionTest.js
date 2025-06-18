5c761529a7854679de43733812c83ddb961861ba
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-18 20:30:14 -0300

    Add menu compression Puppeteer test

---
+++ tests/menuCompressionTest.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: 'new', args: ['--no-sandbox']});
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8080/index.php');
+  await page.waitForSelector('#consolidated-menu-button');
+  await page.click('#consolidated-menu-button');
+  await page.waitForTimeout(500);
+  const hasClass = await page.evaluate(() => document.body.classList.contains('menu-compressed'));
+  if (!hasClass) {
+    console.error('menu-compressed class not added');
+    await browser.close();
+    process.exit(1);
+  }
+  await page.click('#consolidated-menu-button');
+  await page.waitForTimeout(500);
+  const stillHasClass = await page.evaluate(() => document.body.classList.contains('menu-compressed'));
+  if (stillHasClass) {
+    console.error('menu-compressed class not removed');
+    await browser.close();
+    process.exit(1);
+  }
+  console.log('Menu compression class toggled correctly');
+  await browser.close();
+})();
