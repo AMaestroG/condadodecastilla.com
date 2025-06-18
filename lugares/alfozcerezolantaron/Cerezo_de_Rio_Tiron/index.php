8042984e18c17e3fc2461460625bd108112051d3
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-18 19:41:49 -0300

    Remove redundant font links

--- lugares/alfozcerezolantaron/Cerezo_de_Rio_Tiron/index.php
+++ lugares/alfozcerezolantaron/Cerezo_de_Rio_Tiron/index.php
@@ -6,10 +6,6 @@
     <meta name="description" content="Explora la rica historia, las imponentes ruinas y el legado cultural de Cerezo de Río Tirón, un lugar clave en la historia del Condado de Castilla.">
     <link rel="icon" href="/assets/img/escudo.jpg" type="image/jpeg">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700;900&family=Lora:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <link rel="stylesheet" href="/assets/css/epic_theme.css">
 </head>
