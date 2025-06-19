c87db2bdaa119ff48c95da7b176a4fef5ebcd8c1
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-18 21:15:39 -0300

    Add basic HTML structure to pages

--- visitas/visitas.php
+++ visitas/visitas.php
@@ -1,4 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
 <?php require_once __DIR__ . '/../includes/head_common.php'; ?>
+<?php require_once __DIR__ . '/../includes/load_page_css.php'; ?>
+</head>
 <body>
 
     <?php require_once __DIR__ . '/../_header.php'; ?>
