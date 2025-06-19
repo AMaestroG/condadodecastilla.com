fe92fda102639dfb8e523235ee5f283a8fa5ed08
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-19 20:33:15 -0300

    Add missing HTML structure to demo_transparencia_movimiento.php

--- demo_transparencia_movimiento.php
+++ demo_transparencia_movimiento.php
@@ -1,4 +1,8 @@
-<?php require_once __DIR__ . '/includes/head_common.php'; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php require_once __DIR__.'/includes/head_common.php'; ?>
+</head>
 <body class="alabaster-bg">
     <?php require_once __DIR__ . '/_header.php'; ?>
     <section class="demo-hero">
