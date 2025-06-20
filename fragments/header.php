985d991b1073856eadd44a9728829a1290508796
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-20 07:12:20 -0300

    Remove star icon from header menu

--- fragments/header.php
+++ fragments/header.php
@@ -1,7 +1,6 @@
 <div id="cave-mask"></div>
 <div id="fixed-header-elements" style="height: var(--header-footer-height);">
     <div class="header-action-buttons">
-        <img src="/assets/icons/star-of-venus.svg" class="header-icon" alt="Star of Venus icon" />
         <img src="/assets/icons/columna.svg" class="header-icon" alt="Roman column icon" />
         <button id="consolidated-menu-button" data-menu-target="consolidated-menu-items" aria-label="Abrir menú principal" aria-expanded="false" role="button" aria-controls="consolidated-menu-items">☰</button>
         <button id="flag-toggle" data-menu-target="language-panel" aria-label="Seleccionar idioma" aria-expanded="false" role="button" aria-controls="language-panel"><i class="fas fa-flag"></i></button>
