1c0925982537ab50a7982725ee9cc24a10234df2
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-21 14:06:44 -0300

    Remove inline script and defer main.js

--- fragments/footer.php
+++ fragments/footer.php
@@ -29,7 +29,7 @@
         <li>Consulta la agenda de eventos culturales.</li>
     </ul>
 </div>
-<script src="/assets/js/main.js"></script>
+<script defer src="/assets/js/main.js"></script>
 <script src="/assets/js/cave_mask.js"></script>
 <script src="/assets/js/hero.js"></script>
 <script src="/assets/js/scroll-fade.js"></script>
