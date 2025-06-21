aec575d981493e581704a2ddec7552244dfd06e0
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-21 12:15:05 -0300

    Add Scripts admin page with header/footer

--- fragments/menus/admin-menu.php
+++ fragments/menus/admin-menu.php
@@ -4,6 +4,7 @@ echo '<ul id="admin-menu" class="nav-links">';
 if (is_admin_logged_in()) {
     echo '<li><a href="/dashboard/logout.php">Cerrar sesión</a></li>';
     echo '<li><a href="/dashboard/index.php">Panel</a></li>';
+    echo '<li><a href="/scripts_admin.php">Scripts</a></li>';
 } else {
     echo '<li><a href="/dashboard/login.php">Admin</a></li>';
 }
