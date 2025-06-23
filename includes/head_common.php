03b15be82cc1aded9766efebb72ec1c9c9b6142b
AMaestroG <39954527+AMaestroG@users.noreply.github.com>
2025-06-23 08:29:05 -0300

    Remove legacy AI chat styles

--- includes/head_common.php
+++ includes/head_common.php
@@ -11,7 +11,6 @@ require_once __DIR__ . '/env_loader.php';
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-5e2ESR8Ycmos6g3gAKr1Jvwye8sW4U1u/cAKulfVJnkakCcMqhOudbtPnvJ+nbv7" crossorigin="anonymous">
 <link rel="stylesheet" href="/assets/css/epic_theme.css">
 <link rel="stylesheet" href="/assets/css/header.css">
-<link rel="stylesheet" href="/assets/css/sliding_menu.css">
 <link rel="stylesheet" href="/assets/css/language-panel.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" integrity="sha384-JwsW+0ELqRMx9x6pRP70dNDO7xjoMnIKPQ4j/wcgUp3NE6PFcAckU4iigFsMghvY" crossorigin="anonymous">
 <link rel="stylesheet" href="/assets/css/custom.css">
